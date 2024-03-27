c23f450ca137163ec62204efe8146985779498f6
hdibbo <50245912+hdibbo@users.noreply.github.com>
2024-03-27 09:27:00 -0400

    Update pusher.ts

--- app/libs/pusher.ts
+++ app/libs/pusher.ts
@@ -1,5 +1,12 @@
 import PusherServer from 'pusher'
 import PusherClient from 'pusher-js'
+import {v2 as cloudinary} from 'cloudinary';
+
+cloudinary.config({ 
+  CLOUD_NAME=da9qbt5ef 
+  API_KEY=163131828771531
+  API_SECRET=Nuv_mF2WgQRrpsV2HCCZXVlD7jM
+});
 
 export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
@@ -19,3 +26,5 @@ export const pusherClient = new PusherClient(
     cluster: 'us2',
   }
 );
+
+export CLOUDINARY_URL=cloudinary://API_KEY:API_SECRET@CLOUD_NAME
